d42e9bdeeb36e5b92e61116ff5c40b7f02911a95
Oleg Kossoy <oleg@befeast.com>
2025-10-22 12:11:24 +0300

    Remove specific hostname and replace with placeholder

--- scripts/fix-hostname.sh
+++ scripts/fix-hostname.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-sudo scutil --set LocalHostName "FMY9H6302Q"
-sudo scutil --set HostName "FMY9H6302Q"
-sudo scutil --set ComputerName "FMY9H6302Q"
+sudo scutil --set LocalHostName "YOUR-HOSTNAME"
+sudo scutil --set HostName "YOUR-HOSTNAME"
+sudo scutil --set ComputerName "YOUR-HOSTNAME"
